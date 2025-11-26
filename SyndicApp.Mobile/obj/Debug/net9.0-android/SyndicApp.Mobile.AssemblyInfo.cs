67daaa6cd9c68426fd1fa0cc5af82debd24b5b90
SyndycApp <syndycapp@gmail.com>
2025-11-26 18:02:09 +0100

    fix affichage proprio

--- SyndicApp.Mobile/obj/Debug/net9.0-android/SyndicApp.Mobile.AssemblyInfo.cs
+++ SyndicApp.Mobile/obj/Debug/net9.0-android/SyndicApp.Mobile.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SyndicApp.Mobile")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+dcdbc8976c9e2fb8cac04c77adca3805cdd2bd2e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+60290bf93ba72be33819f7004b1bd931128eeb45")]
 [assembly: System.Reflection.AssemblyProductAttribute("SyndicApp.Mobile")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SyndicApp.Mobile")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
